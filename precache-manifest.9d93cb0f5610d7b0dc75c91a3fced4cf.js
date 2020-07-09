72e805b22bbdaf38dcd86a18883c217f2f4bfc2f
alexVezer <alexartimon@gmail.com>
2020-07-09 19:29:22 +0300

    Updates

---
+++ precache-manifest.9d93cb0f5610d7b0dc75c91a3fced4cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09cdc708588e0065c9bb03dd8d3656f4",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "2cec397862b74385c811",
+    "url": "/portofolio/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "199031417a40dca0d66a",
+    "url": "/portofolio/static/css/main.e50a6d51.chunk.css"
+  },
+  {
+    "revision": "2cec397862b74385c811",
+    "url": "/portofolio/static/js/2.7a6c177f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portofolio/static/js/2.7a6c177f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "199031417a40dca0d66a",
+    "url": "/portofolio/static/js/main.510184c7.chunk.js"
+  },
+  {
+    "revision": "19d1097e3554bac1aab3",
+    "url": "/portofolio/static/js/runtime-main.0168f7f7.js"
+  }
+]);
